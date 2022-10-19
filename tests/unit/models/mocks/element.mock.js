2d9791529cec2600b59054dd07926d34d212ac10
Icaro Joel <icaromoura.programador@gmail.com>
2022-10-19 14:03:09 -0300

    make a test of elements find functions

---
+++ tests/unit/models/mocks/element.mock.js
@@ -0,0 +1,82 @@
+const allElements = [
+  {
+    id: 1,
+    element: 'Bug',
+  },
+  {
+    id: 2,
+    element: 'Dark',
+  },
+  {
+    id: 3,
+    element: 'Dragon',
+  },
+  {
+    id: 4,
+    element: 'Eletric',
+  },
+  {
+    id: 5,
+    element: 'Fairy',
+  },
+  {
+    id: 6,
+    element: 'Figting',
+  },
+  {
+    id: 7,
+    element: 'Fire',
+  },
+  {
+    id: 8,
+    element: 'Flying',
+  },
+  {
+    id: 9,
+    element: 'Ghost',
+  },
+  {
+    id: 10,
+    element: 'Grass',
+  },
+  {
+    id: 11,
+    element: 'Ground',
+  },
+  {
+    id: 12,
+    element: 'Ice',
+  },
+  {
+    id: 13,
+    element: 'Normal',
+  },
+  {
+    id: 14,
+    element: 'Poison',
+  },
+  {
+    id: 15,
+    element: 'Psychic',
+  },
+  {
+    id: 16,
+    element: 'Rock',
+  },
+  {
+    id: 20,
+    element: 'si',
+  },
+  {
+    id: 17,
+    element: 'Steel',
+  },
+  {
+    id: 18,
+    element: 'Water',
+  },
+];
+
+module.exports = {
+  allElements,
+};
